9d33f0ca3266c85945754ab23292c3c6b22f636c
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-13 20:29:19 +0300

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 describe( "index.js", () => {
   it( 'should change the text on the page when DOM content has loaded', function () {
-    expect( document.querySelector( 'p' )
+    expect( document.querySelector('p')
         .textContent )
       .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
 
@@ -14,3 +14,4 @@ describe( "index.js", () => {
   } )
 
 } )
+
